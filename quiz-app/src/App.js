e42a7780a7db91ead9f224a1cc1d04b505e12317
iryna_ostapuk <irusya.rina@gmail.com>
2021-09-29 21:47:56 +0300

    add server folder

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import Layout from './containers/Layout/Layout';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home/Home';
 
 function App() {
---
+++ quiz-app/src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Home.scss';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="text-content">
+                <h1>Welcome to us!</h1>
+                <p>Do you love music and can't imagine your life without it?
+                    So, you are in the right place! Here you can combine the useful with the pleasant.
+                    Let's start lerning English with favorite songs!
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
